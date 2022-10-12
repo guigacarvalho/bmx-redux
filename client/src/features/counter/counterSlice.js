d907276a25430d19d2aca0b72411e43669c82682
forrestmorrison <forrestmorrison@gmail.com>
2022-10-12 15:09:09 -0500

    added Counter component to app, Counter functioning properly, no errors

--- client/src/features/counter/counterSlice.js
+++ client/src/features/counter/counterSlice.js
@@ -4,7 +4,7 @@ const initialState = {
     count: 0
 }
 
-export const counterSlice = create createSlice({
+export const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
