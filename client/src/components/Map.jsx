3577ab191390d3f01b21f71a99e23e40d3327e76
forrestmorrison <forrestmorrison@gmail.com>
2023-01-09 21:11:27 -0600

    imported GoogleMapReact and added location const

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,4 +1,11 @@
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import GoogleMapReact from 'google-map-react'
+import '../map.css'
+
+const location = {
+  address: '12257 Kuykendahl Rd, Houston, TX 77067',
+  lat: 29.95862,
+  lng: -95.42057,
+}
 
 export function Map() {
   return (
