625de3832f0c9b49d4e63ec9ce7acdcf6c050a56
forrestmorrison <forrestmorrison@gmail.com>
2023-01-09 19:50:08 -0600

    added logo to header & styled

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
+import bmxlogo from '../images/bmxlogo.jpg'
 
 const Header = () => {
 
     return (
         <header className='header'>
             <div className='logo'>
-                <Link to='/'>bmxparks.org</Link>
+                <img src={bmxlogo} alt='bmxlogo' className='bmxlogo'/>
+                <Link to='/'><h1>bmxparks.org</h1></Link>
             </div>
             <ul class="nav-links">
                 <li>
