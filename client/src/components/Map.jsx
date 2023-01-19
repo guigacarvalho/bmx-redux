4ceff684d9f25c3fd4fd2b59445c3b7f7349e271
forrestmorrison <forrestmorrison@gmail.com>
2023-01-19 15:29:56 -0600

    setup ParkList

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,11 +1,10 @@
-import GoogleMapReact from 'google-map-react'
 import fakemap from '../images/fakemap.jpg'
 
-const location = {
-  address: '12257 Kuykendahl Rd, Houston, TX 77067',
-  lat: 29.95862,
-  lng: -95.42057,
-}
+// const location = {
+//   address: '12257 Kuykendahl Rd, Houston, TX 77067',
+//   lat: 29.95862,
+//   lng: -95.42057,
+// }
 
 export function Map() {
   return (
--- client/src/components/ParkList.jsx
+++ client/src/components/ParkList.jsx
@@ -1,10 +1,25 @@
 import ParkCard from "./ParkCard"
 import ParkContext from "../context/ParkContext"
+import { useContext } from "react"
 
 const ParkList = () => {
-  return (
-    <div>ParkList</div>
-  )
+
+    const {parks} = useContext(ParkContext)
+
+    if (!parks || parks.length === 0) {
+        return <p>no parks added yet</p>
+    }
+
+    return (
+        <div className="park-list">
+            {parks.map((card) => (
+                <ParkCard
+                    key={parks.id}
+                    park={park}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default ParkList
