e77f74453ddbc5de3ba38115cec9f4b3886d2983
forrestmorrison <forrestmorrison@gmail.com>
2023-01-18 20:05:03 -0600

    added input form to AddParkForm modal

--- client/src/components/AddParkForm.jsx
+++ client/src/components/AddParkForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FaTimes } from 'react-icons/fa'
 
 const AddParkForm = () => {
 
@@ -22,13 +23,35 @@ const AddParkForm = () => {
 
       {modal && (
         <div className="modal">
-          <div className="overlay" onClick={toggleModal}>
-
-          </div>
+          <div className="overlay" onClick={toggleModal}></div>
           <div className="modal-content">
-            <h2>Hello</h2>
-            <p>This is a modal</p>
-            <button onClick={toggleModal} className="close-modal">x</button>
+            <form>
+              <h2>New Park</h2>
+              <div className="input-group">
+                <label>park name</label>
+                <input 
+                  type="text" 
+                  placeholder="park name"
+                />
+                <br />
+                <label>park address</label>
+                <input 
+                  type="text" 
+                  placeholder="park address"
+                />
+                <br />
+                <label>park type</label>
+                <input 
+                  type="text" 
+                  placeholder="park type"
+                />
+                <br />
+                <button type="submit">Add Park</button>
+              </div>
+            </form>
+            <button onClick={toggleModal} className="close-modal">
+              <FaTimes color="red" />
+            </button>
           </div>
         </div>
       )}
